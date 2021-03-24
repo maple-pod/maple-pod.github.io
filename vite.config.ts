cce5023bb53e5b0491c58664784892255bc9783d
DevilTea <ch870814@gmail.com>
2021-03-24 09:43:46 +0800

    Add vite-plugin-stylelint

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { join } from 'path'
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import WindiCSS from 'vite-plugin-windicss'
+import Stylelint from '@amatlash/vite-plugin-stylelint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +13,10 @@ export default defineConfig({
   },
   plugins: [
     Vue(),
-    WindiCSS()
+    WindiCSS(),
+    Stylelint({
+      exclude: /(node_modules)|(windi\.css)/
+    })
   ],
   ssgOptions: {
     script: 'async'
