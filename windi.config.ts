f48e1df51c90cbc77780b0e951867563d0748f25
DevilTea <ch870814@gmail.com>
2021-03-24 23:42:11 +0800

    Update windi config

--- windi.config.ts
+++ windi.config.ts
@@ -1,13 +1,31 @@
 import { defineConfig } from 'vite-plugin-windicss'
-import colors from'windicss/colors'
+import colors from 'windicss/colors'
 
 export default defineConfig({
   darkMode: 'class',
   theme: {
+    screens: {
+      xsOnly: { max: 575 },
+      smAndDown: { max: 767 },
+      smOnly: { min: 576, max: 767 },
+      smAndUp: { min: 576 },
+      mdAndDown: { max: 991 },
+      mdOnly: { min: 768, max: 991 },
+      mdAndUp: { min: 768 },
+      lgAndDown: { max: 1199 },
+      lgOnly: { min: 992, max: 1199 },
+      lgAndUp: { min: 992 },
+      xlAndDown: { max: 1399 },
+      xlOnly: { min: 1200, max: 1399 },
+      xlAndUp: { min: 1200 },
+      xxlOnly: { min: 1400 }
+    },
     colors: {
       ...colors,
       'app-light': '#DFDFDF',
-      'app-dark': '#333333'
+      'app-dark': '#333333',
+      primary: '#E36262',
+      secondary: '#B5B5B5'
     }
   }
 })
